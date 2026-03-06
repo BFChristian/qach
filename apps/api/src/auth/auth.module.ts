8d8635aefdffb2ed8d3fce85c5fa3e6914827dfd
Christian <b.christian.fuentes@gmail.com>
2026-03-05 22:21:09 -0300

    auth module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '2h' },
+      }),
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
